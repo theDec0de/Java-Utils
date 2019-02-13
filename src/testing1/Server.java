0e5d00f60b19abaa4188d19ace4756244a8f280a
theDec0de <kjwilcox02@gmail.com>
2019-02-13 06:08:43 -0800

    Added Server class

---
+++ src/testing1/Server.java
@@ -0,0 +1,11 @@
+package testing1;
+
+import java.net.*;
+import java.io.*;
+
+public class Server {
+	private ServerSocket serverSocket;
+	private Socket clientSocket;
+	private PrintWriter out;
+	private BufferedReader in;
+}
