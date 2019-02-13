a1cef6b934ac78719ff3c1efe6360910c2014088
theDec0de <kjwilcox02@gmail.com>
2019-02-12 16:44:17 -0800

    Changed Angle Class functionality

--- src/testing1/Angle.java
+++ src/testing1/Angle.java
@@ -1,11 +1,11 @@
 package testing1;
 
 public class Angle {
-	double degrees;
-	
-	Angle(double degrees) {
-		this.degrees = degrees;
+	public static double findMissingSupp(double a1) {
+		return 180 - a1;
 	}
 	
-	
+	public static double findMissingComp(double a1) {
+		return 90 - a1;
+	}
 }
